ce9033f726ab85c440bbf65b4b1d6f013d37e205
Samed <samed.bille@gmail.com>
2017-08-05 23:38:02 +0300

    Bölüm19
    Bölüm 19 projesi

---
+++ App1/App1/FramedTextPage.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace App1
+{
+    public class FramedTextPage : ContentPage
+    {
+        public FramedTextPage() {
+            StackLayout stackLayout = new StackLayout();
+            // Döngü sayesinde renk yapısı alanları.
+            foreach (FieldInfo info in typeof(Color).GetRuntimeFields())
+            {
+                // Eski (yani hatalı yazıldı) renkleri atlayın.
+                if (info.GetCustomAttribute<ObsoleteAttribute>() != null)
+                    continue;
+                if (info.IsPublic &&
+                info.IsStatic &&
+                info.FieldType == typeof(Color))
+                {
+                    stackLayout.Children.Add(
+                    CreateColorView((Color)info.GetValue(null), info.Name));
+                }
+            }
+            // Döngü sayesinde renk yapısı özellikleri.
+            foreach (PropertyInfo info in typeof(Color).GetRuntimeProperties())
+            {
+                MethodInfo methodInfo = info.GetMethod;
+                if (methodInfo.IsPublic &&
+                methodInfo.IsStatic &&
+                methodInfo.ReturnType == typeof(Color))
+                {
+                    stackLayout.Children.Add(
+                    CreateColorView((Color)info.GetValue(null), info.Name));
+                }
+            }
+            Padding = new Thickness(5, Device.OnPlatform(20, 5, 5), 5, 5);
+            // StackLayout'u bir ScrollView'e yerleştirin.
+            Content = new ScrollView
+            {
+                Content = stackLayout
+            };
+
+        }
+        
+        View CreateColorView(Color color, string name)
+        {
+            return new Frame
+            {
+                OutlineColor = Color.Accent,
+                Padding = new Thickness(5),
+                Content = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal,
+                    Spacing = 15,
+                    Children =
+                 {
+                 new BoxView
+                 {
+                 Color = color,
+                 
+                 },
+                new Label
+                {
+                 Text = name,
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                 FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.Center,
+                HorizontalOptions = LayoutOptions.StartAndExpand
+                 },
+                 new StackLayout
+                 {
+                 Children =
+                {
+                 new Label
+                {
+                 Text = String.Format("{0:X2}-{1:X2}-{2:X2}",
+                 (int)(255 * color.R),
+                 (int)(255 * color.G),
+                (int)(255 * color.B)),
+                 VerticalOptions = LayoutOptions.CenterAndExpand,
+                IsVisible = color != Color.Default
+                 },
+                new Label
+                {
+                 Text = String.Format("{0:F2}, {1:F2}, {2:F2}",
+                 color.Hue,
+                color.Saturation,
+                color.Luminosity),
+                 VerticalOptions = LayoutOptions.CenterAndExpand,
+                 IsVisible = color != Color.Default
+                 }
+                 },
+                HorizontalOptions = LayoutOptions.End
+                 }
+                 }
+                }
+            };
+        }
+    }
+}
